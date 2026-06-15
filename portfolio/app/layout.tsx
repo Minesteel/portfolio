19d61eb8e0c7a5411ec6991170953cc03b3c83d9
Loris <loris.pineaud@gmail.com>
2026-06-15 12:48:40 +0200

    feat: refactor layout by integrating LayoutClient functionality directly into RootLayout and removing LayoutClient component

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,13 +1,12 @@
-import LayoutClient from '@/components/LayoutClient'
+"use client"
+
+import { useState } from 'react'
+import Header from '@/components/Header'
+import Sidenav from '@/components/Sidenav'
 import { LanguageProvider } from '@/contexts/LanguageContext'
 import { Geist, Geist_Mono } from "next/font/google";
-import type { Metadata } from 'next'
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Mon portfolio',
-}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,12 +18,30 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  // Par défaut, on peut décider qu'elle est ouverte sur grand écran
+  const [isCollapsed, setIsCollapsed] = useState(true)
+
+  const toggleSidebar = () => setIsCollapsed(!isCollapsed)
+
   return (
     <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <LanguageProvider>
-          <LayoutClient>{children}</LayoutClient>
+          <div className="flex flex-col h-screen">
+            <Header isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
+
+            <div className="flex flex-1 overflow-hidden">
+              <Sidenav isCollapsed={isCollapsed} />
+
+              <main className="flex-1 overflow-y-auto">
+                <div className="">
+                  {children}
+                </div>
+              </main>
+            </div>
+          </div>
         </LanguageProvider>
       </body>
     </html>
